d5d436e4d0c631e21d7b18d4887d0ba723159595
pathak0311 <36003493+pathak0311@users.noreply.github.com>
2022-08-31 10:11:07 +0530

    add podcast download feature
    The Boring Flutter Development Show Ep. 33 completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,68 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
+import 'package:http/http.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:webfeed/webfeed.dart';
+
+const url = 'https://itsallwidgets.com/podcast/feed';
+final pathSuffix = 'dashcast/downloads';
+
+Future<String> _getDownloadPath(String filename) async {
+  final dir = await getApplicationDocumentsDirectory();
+  final prefix = dir.uri.path;
+
+  final absolutePath = join(prefix, filename);
+  print(absolutePath);
+  return absolutePath;
+}
+
+class Podcast with ChangeNotifier {
+  RssFeed _feed = RssFeed();
+  RssItem _selectedItem = RssItem();
+  Map<String, bool> downloadStatus = {};
+
+  RssFeed get feed => _feed;
+
+  void parse(String url) async {
+    final res = await get(Uri.parse(url));
+    final xmlStr = res.body;
+    _feed = RssFeed.parse(xmlStr);
+    notifyListeners();
+  }
+
+  RssItem get selectedItem => _selectedItem;
+
+  set selectedItem(RssItem value) {
+    _selectedItem = value;
+    notifyListeners();
+  }
+
+  void download(RssItem item) async {
+    final client = Client();
+
+    final req = Request("GET", Uri.parse(item.guid!));
+    final res = await client.send(req);
+
+    if (res.statusCode != 200)
+      throw Exception('Unexpected Error : ${res.statusCode}');
+
+    final file = File(await _getDownloadPath(split(item.guid!).last));
+
+    // res.stream.listen((bytes) {
+    //   print(bytes.length);
+    // });
+
+    res.stream.pipe(file.openWrite()).whenComplete(() {
+      print('Complete');
+    });
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -13,34 +73,96 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'The Boring Show!',
-      home: BoringPage(),
+    return ChangeNotifierProvider(
+      create: (context) => Podcast()..parse(url),
+      child: const MaterialApp(
+        title: 'The Boring Show!',
+        home: EpisodesPage(),
+      ),
     );
   }
 }
 
-class BoringPage extends StatelessWidget {
-  const BoringPage({Key? key}) : super(key: key);
+class EpisodesPage extends StatelessWidget {
+  const EpisodesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: DashCastApp()));
+    return Scaffold(body: Consumer<Podcast>(
+      builder: (context, podcast, _) {
+        return EpisodeListView(rssFeed: podcast.feed);
+      },
+    ));
   }
 }
 
-class DashCastApp extends StatelessWidget {
-  const DashCastApp({Key? key}) : super(key: key);
+class EpisodeListView extends StatelessWidget {
+  const EpisodeListView({
+    Key? key,
+    required this.rssFeed,
+  }) : super(key: key);
+
+  final RssFeed rssFeed;
 
   @override
   Widget build(BuildContext context) {
+    return ListView(
+      children: rssFeed.items!
+          .map((e) => ListTile(
+                title: Text(e.title!),
+                subtitle: Text(
+                  e.description!,
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.download),
+                  onPressed: () {
+                    Provider.of<Podcast>(context, listen: false).download(e);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Downloading ${e.title}')));
+                  },
+                ),
+                onTap: () {
+                  Provider.of<Podcast>(context, listen: false).selectedItem = e;
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const PlayerPage()));
+                },
+              ))
+          .toList(),
+    );
+  }
+}
+
+class PlayerPage extends StatelessWidget {
+  const PlayerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(Provider.of<Podcast>(context).selectedItem.title!),
+        ),
+        body: const SafeArea(child: const Player()));
+  }
+}
+
+class Player extends StatelessWidget {
+  const Player({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final podcast = Provider.of<Podcast>(context, listen: false);
     return Column(
       children: [
+        Flexible(flex: 5, child: Image.network(podcast.feed.image!.url!)),
         Flexible(
-          flex: 9,
-          child: Placeholder(),
+          flex: 4,
+          child: SingleChildScrollView(
+            child: Text(podcast.selectedItem.description!),
+          ),
         ),
-        Flexible(flex: 2, child: AudioControls())
+        const Flexible(flex: 2, child: AudioControls())
       ],
     );
   }
@@ -53,7 +175,7 @@ class AudioControls extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        PlaybackButtons(),
+        const PlaybackButtons(),
       ],
     );
   }
@@ -67,7 +189,7 @@ class PlaybackButtons extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        PlaybackButton(),
+        const PlaybackButton(),
       ],
     );
   }
@@ -88,9 +210,6 @@ class _PlaybackButtonState extends State<PlaybackButton> {
   int playPosition = 0;
 
   StreamSubscription? _mPlayerSubscription;
-
-  final url =
-      "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Surf%20Shimmy.mp3";
 
   @override
   void initState() {
@@ -116,10 +235,16 @@ class _PlaybackButtonState extends State<PlaybackButton> {
     await _mPlayer.stopPlayer();
   }
 
-  void _play() async {
-    await _mPlayer.startPlayer(fromURI: url, codec: Codec.mp3, whenFinished: () {
-      setState(() {});
-    });
+  void _play(String url) async {
+    print(url);
+    url =
+        "/data/user/0/in.theweekenddeveloper.dashcast/app_flutter/episode-33.mp3";
+    await _mPlayer.startPlayer(
+        fromURI: url,
+        codec: Codec.mp3,
+        whenFinished: () {
+          setState(() {});
+        });
     setState(() {});
   }
 
@@ -129,6 +254,7 @@ class _PlaybackButtonState extends State<PlaybackButton> {
 
   @override
   Widget build(BuildContext context) {
+    final item = Provider.of<Podcast>(context, listen: false).selectedItem;
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -142,20 +268,22 @@ class _PlaybackButtonState extends State<PlaybackButton> {
             }),
         Row(
           children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.fast_rewind)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.fast_rewind)),
             IconButton(
                 onPressed: () {
                   if (_isPlaying) {
                     _stop();
                   } else {
-                    _play();
+                    _play(item.guid!);
                   }
                   setState(() {
                     _isPlaying = !_isPlaying;
                   });
                 },
-                icon: (_isPlaying) ? Icon(Icons.stop) : Icon(Icons.play_arrow)),
-            IconButton(onPressed: () {}, icon: Icon(Icons.fast_forward)),
+                icon: (_isPlaying)
+                    ? const Icon(Icons.stop)
+                    : const Icon(Icons.play_arrow)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.fast_forward)),
           ],
         ),
         Text('$playPosition')
